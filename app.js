5c4efe3a5b4e66e76418442a97427be01985aaea
MouadBAni <mouad-mb@outlook.com>
2023-02-20 10:43:18 +0100

    use import instead of require

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express');
+import express from 'express'
 const app = express();
-const dotenv = require('dotenv')
+import dotenv from 'dotenv'
 dotenv.config();
 
 app.use(express.json())
