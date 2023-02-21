9cbb3feab06ceba54234cdde414de62e7a693ea8
MouadBAni <mouad-mb@outlook.com>
2023-02-21 11:03:41 +0100

    add test route and error handler

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const app = express();
 import dotenv from 'dotenv'
 dotenv.config();
 import helmet from 'helmet'
+import {errorHandler} from './src/middelware/index.middelware.js'
+import {testRouter} from './src/routes/index.routes.js';
 
 app.use(helmet()); // adding security layers 
 app.use(express.json())
 app.use(express.urlencoded({ extended:true }));
 
+app.use('/test', testRouter)
+
+app.use(errorHandler) // All routes should be defined above this line in order to catch exceptions
 
 const port = process.env.PORT || 3000;
 
